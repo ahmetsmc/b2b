4e7d950b8d801545090cb91e13bdea74eb5cc2eb
ahmersmc <ahmetalpersam@gmail.com>
2024-12-09 23:31:46 +0300

    basic confirmation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,12 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +24,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Schema::defaultStringLength(191);
+        date_default_timezone_set('Europe/Istanbul');
+        Carbon::setLocale(config('app.locale'));
+        setlocale(LC_TIME, LaravelLocalization::getCurrentLocaleRegional());
+        Paginator::defaultView('vendor.pagination.default');
+        Paginator::defaultSimpleView('vendor.pagination.default');
+
+        if (config('app.url') == "http://localhost:8000"){
+            Mail::alwaysTo('ahmetalpersam@gmail.com');
+        }
     }
 }
