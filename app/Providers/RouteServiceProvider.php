36730dfec3270b4a2c0a92883cbd4474b9cccf54
ahmersmc <ahmetalpersam@gmail.com>
2024-12-18 00:48:34 +0300

    update api routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -29,8 +29,8 @@ class RouteServiceProvider extends ServiceProvider
         });
 
         $this->routes(function () {
-            Route::middleware('api')
-                ->prefix('api')
+            Route::domain('api.b2b.test')
+                ->middleware('api')
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
